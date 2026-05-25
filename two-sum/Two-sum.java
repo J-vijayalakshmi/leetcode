299fb7dd53ce8651e4840e4e3efa180e3cadc7da
vijji <vijji@example.com>
2026-05-25 15:57:46 +0530

    Add Two-sum solution implementation

---
+++ two-sum/Two-sum.java
@@ -0,0 +1,18 @@
+class Two-sum {
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> hm = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(hm.containsKey(target-nums[i])){
+                return new int[] {hm.get(target - nums[i]),i};
+            }
+            hm.put(nums[i],i);
+        }
+        return new int[] {-1,-1};
+    }
+    public static void main(String aregs[]){
+        int[] nums = new int{4,5,6,7,8,9,0};
+        int target = 9;
+        twoSum(nums, target);
+    }
+}
